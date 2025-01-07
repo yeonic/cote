0eb4ee8b4a187fe4933518c8c11865661699d440
김종연 <bellopenerr@gmail.com>
2025-01-07 09:59:01 +0900

    initial commit

---
+++ src/Suicasui_31.java
@@ -0,0 +1,19 @@
+public class Suicasui_31 {
+
+  public static void main(String[] args) {
+    System.out.println(solution(7));
+  }
+
+  static String solution(int n) {
+    StringBuilder result = new StringBuilder();
+
+    for (int i=1; i<=n; i++) {
+      if (i%2 == 0) {
+        result.append("박");
+      } else {
+        result.append("수");
+      }
+    }
+    return result.toString();
+  }
+}
---
+++ src/Three_Musketeers_42.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Three_Musketeers_42 {
+
+  public static void main(String[] args) {
+    System.out.println(solution(new int[]{0, 0, 0, 0, 0, 0}));
+
+  }
+
+  public static int solution(int[] number) {
+    int cnt = 0;
+
+    Arrays.sort(number);
+    for (int i = 0; i < number.length - 2; i++) {
+      if (number[i] > 0) {
+        break;
+      }
+
+      for (int j = i + 1; j < number.length - 1; j++) {
+        for (int k = j + 1; k < number.length; k++) {
+          if (number[i] + number[j] + number[k] == 0) {
+            cnt++;
+          }
+        }
+      }
+    }
+
+    return cnt;
+  }
+}
+
+// [-2, 3, 0, 2, -5] -> [-5, -2, 0, 2, 3]
+// [-3, -2, -1, 0, 1, 2, 3]
+// [-1, 1, -1, 1]
