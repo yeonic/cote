5cf6b966a4116922bfa34f073e60f53802c4b766
김종연 <bellopenerr@gmail.com>
2025-05-06 14:48:22 +0900

    백준

---
+++ src/boj/Boj11729.java
@@ -0,0 +1,13 @@
+package boj;
+
+public class Boj11729 {
+    public static void main(String[] args) {
+
+    }
+
+    private static void hanoi(int a, int b, int n) {
+        //1 n = 1   1 3
+        //3 n = 2   1 2, 1 3, 2 3
+        //7 n = 3   1 3, 1 2, 3 2, 1 3, 2 1, 2 3, 1 3
+    }
+}
