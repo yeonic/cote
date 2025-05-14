77dc2751101c41199fd9757f8d5c75f734cbafec
김종연 <bellopenerr@gmail.com>
2025-05-14 11:01:40 +0900

    백준 1655: 가운데를 말해요

--- src/boj/Boj1655.java
+++ src/boj/Boj1655.java
@@ -1,4 +1,42 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Boj1655 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int round = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < round; i++) {
+            int curr = Integer.parseInt(br.readLine());
+
+            if (maxHeap.isEmpty() && minHeap.isEmpty()) {
+                maxHeap.offer(curr);
+                System.out.println(curr);
+                continue;
+            }
+
+            if (maxHeap.size() > minHeap.size()) {
+                minHeap.offer(curr);
+            } else {
+                maxHeap.offer(curr);
+            }
+
+            if (maxHeap.peek() > minHeap.peek()) {
+                Integer minOfLargerHalf = minHeap.poll();
+                Integer maxOfSmallerHalf = maxHeap.poll();
+                minHeap.offer(maxOfSmallerHalf);
+                maxHeap.offer(minOfLargerHalf);
+            }
+            System.out.println(maxHeap.peek());
+        }
+    }
 }
